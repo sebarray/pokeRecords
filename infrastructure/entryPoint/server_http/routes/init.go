98bf8c82f3b131bf322a059a8f4c26f8de9a533a
sebarray <sebarray98@gmail.com>
2024-06-18 19:04:45 -0300

    arch2

--- infrastructure/entryPoint/server_http/routes/init.go
+++ infrastructure/entryPoint/server_http/routes/init.go
@@ -1,19 +1,10 @@
 package routes
 
 import (
-	usecases "app/pkg/handlers/useCases"
-
 	"github.com/labstack/echo/v4"
 )
 
 func Init(e *echo.Echo) {
 
 	e.GET("/register", Register)
-}
-
-func Register(e echo.Context) error {
-
-	h := usecases.NewHandler()
-	err := h.Auth.Register("user")
-	return err
 }
